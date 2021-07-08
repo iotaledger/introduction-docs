a44e8a66468e68473f8263c10d67fc5276aa49f0
lucastortora <lucas.tortora@iota.org>
2021-07-08 18:00:47 +0200

    * Updated docusaurus.config.js

--- docOps/docusaurus.config.js
+++ docOps/docusaurus.config.js
@@ -7,8 +7,8 @@ module.exports = {
   tagline: 'Official IOTA Chrysalis Software',
   url: 'https://chrysalis.docs.iota.org/',
   baseUrl: '/chrysalis/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+  onBrokenLinks: 'warning',
+  onBrokenMarkdownLinks: 'warning',
   favicon: '/img/logo/favicon.ico',
   organizationName: 'iotaledger', // Usually your GitHub org/user name.
   projectName: 'Chrysalis', // Usually your repo name.
