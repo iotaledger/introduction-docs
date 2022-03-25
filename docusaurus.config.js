3f58b8c48547ac7bf6ce6ba3f8c1b9a5f6218d22
Dr-Electron <dr-electr0n@protonmail.com>
2022-03-25 23:46:54 +0100

    Prepare for new wiki build system

---
+++ docusaurus.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    title: 'Chrysalis Documentation',
+    url: '/',
+    baseUrl: '/',
+    themes: ['@docusaurus/theme-classic'],
+    plugins: [
+        [
+            '@docusaurus/plugin-content-docs',
+            {
+                id: 'chrysalis-docs',
+                path: path.resolve(__dirname, './docs'),
+                routeBasePath: 'chrysalis-docs',
+                sidebarPath: path.resolve(__dirname, './sidebars.js'),
+                remarkPlugins: [require('remark-code-import'), require('remark-import-partial'), require('remark-remove-comments')],
+            }
+        ],
+    ],
+    staticDirectories: [path.resolve(__dirname, './static')],
+};
