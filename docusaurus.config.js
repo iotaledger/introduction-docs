9efe70959e5b8b639dd6f6ac46b7e30a81b4adc4
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-11 20:39:02 +0000

    Cleanup PR and update cli

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,12 +10,13 @@ module.exports = {
             '@docusaurus/plugin-content-docs',
             {
                 id: 'chrysalis-docs',
-                path: path.resolve(__dirname, './docs'),
+                path: path.resolve(__dirname, 'docs'),
                 routeBasePath: 'chrysalis-docs',
-                sidebarPath: path.resolve(__dirname, './sidebars.js'),
+                sidebarPath: path.resolve(__dirname, 'sidebars.js'),
+                editUrl: 'https://github.com/iotaledger/chrysalis-docs/edit/main',
                 remarkPlugins: [require('remark-code-import'), require('remark-import-partial'), require('remark-remove-comments')],
             }
         ],
     ],
-    staticDirectories: [path.resolve(__dirname, './static')],
+    staticDirectories: [path.resolve(__dirname, 'static')],
 };
