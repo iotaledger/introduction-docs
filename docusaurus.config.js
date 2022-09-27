d40bf0a0bba7d1868c83e364e6de9f3862c587b4
Dr-Electron <dr-electr0n@protonmail.com>
2022-09-27 22:51:39 +0200

    Remove docs version info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,19 +5,12 @@ module.exports = {
         [
             '@docusaurus/plugin-content-docs',
             {
-                id: 'introduction-docs-develop',
+                id: 'introduction-docs',
                 path: path.resolve(__dirname, 'docs'),
                 routeBasePath: 'introduction',
                 sidebarPath: path.resolve(__dirname, 'sidebars.js'),
                 editUrl: 'https://github.com/iotaledger/chrysalis-docs/edit/develop/docs',
                 remarkPlugins: [require('remark-code-import'), require('remark-import-partial'), require('remark-remove-comments')],
-                versions: {
-                    current: {
-                        label: 'Develop',
-                        path: 'develop',
-                        badge: true
-                    },
-                },
             }
         ],
     ],
